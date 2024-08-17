c1519a1f29402b7208b262c6f2fac293e5b2d945
jpntc <judepierre4477@gmail.com>
2024-08-17 18:00:24 -0400

    finished home page layout

--- frontend/app/components/Input.jsx
+++ frontend/app/components/Input.jsx
@@ -1,12 +1,31 @@
 "use client"
-import React from 'react'
+import React, {useState} from 'react'
 
 const Input = () => {
-  return (
-    <div className="w-full h-full border-red-300">
-        TextInput
-    </div>
-  )
-}
+  const [input, setInput] = useState("")
 
+  const handleInput = (e)=>{
+    setInput(e.target.value)
+  }
+  return ( 
+    <div className="p-2 mx-2 flex flex-col bg-slate-300 w-full rounded-md">
+      
+      <div className="w-full">
+        <div className="flex min-h-80 md:min-h-96 ">Results</div>
+        <div className="flex items-center">
+          <textarea
+            className="overflow-y-auto w-full mx-2 rounded-sm max-h-16"
+            type="text"
+            placeholder={"Enter a complaint to be processed here."}
+            onChange={(e)=>handleInput(e)}
+            
+          ></textarea>
+          <button className="bg-green-600 rounded-md font-bold p-2 h-fit" onClick={()=>{console.log(input)}}>
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default Input
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -11,18 +11,15 @@ export default function Home() {
  const [pane, setPane] = useState(panes[0])
  const [inputPane, setCurrentInputPane] = useState(inputOptions[0])
   return (
-    <main className="flex min-h-screen flex-col   md:px-24">
+    <main className="flex min-h-screen flex-col md:px-24">
       <Navbar className="fixed top-0 left-0 border" />
-        <div className="flex px-4">
+        <div className="flex px-4 mt-10">
           <Menu method={setPane} options={panes} currentPane={pane} />
           {pane == panes[0] ? (
             <Input />
           ) : (
             <ComplaintsPane />
           )}
-          <div className="">
-
-          </div>
         </div>
     </main>
   );
