f829f1b527041dbdeeef3b68ad069fddee3d5f8c
jpntc <judepierre4477@gmail.com>
2024-08-17 18:18:24 -0400

    frontend with home page, login, and signup

--- frontend/app/components/Menu.jsx
+++ frontend/app/components/Menu.jsx
@@ -14,7 +14,6 @@ const Menu = ({ method, options, currentPane }) => {
             ))
               }
             </div>
-         
         ):<></>
         }
         {isOpen?(<div className="hidden">{currentPane}</div>): <div className="rounded-md font-bold w-fit">{currentPane}</div>}
